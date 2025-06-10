4fc1cd9b116129bea30280c316a1937ea4c9703e
Guilherme Kwaczynski <guilhermetrajanoski@gmail.com>
2025-06-09 21:21:56 -0300

    JavaScript aula 1

---
+++ 03 - JavaScript/01 - Introdução/exemplo01.js
@@ -0,0 +1,2 @@
+
+alert("Hello World do arquivo")
---
+++ 03 - JavaScript/01 - Introdução/exemplo6.js
@@ -0,0 +1,6 @@
+let num = 10
+console.log(typeof (num))
+num = String(num)
+console.log(typeof (num))
+num = Boolean(num)
+console.log(typeof (num))
